558e6bc968e353fdc3ef0d63bdbd192c5c3d1350
oliver-geht-dich-nichts-an <superdaddy1963@googlemail.com>
2016-03-20 08:27:50 +0100

    Added files via upload

---
+++ craftings.lua
@@ -0,0 +1,144 @@
+minetest.register_craft({
+	output = 'better_swords:bigsword_wood',
+	recipe = {
+		{'', 'group:wood', 'group:wood'},
+		{'group:wood', 'group:wood', 'group:wood'},
+		{'group:stick', 'group:wood', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:knife_wood',
+	recipe = {
+		{'', '', ''},
+		{'', 'group:wood', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:bigsword_stone',
+	recipe = {
+		{'', 'group:stone','group:stone'},
+		{'group:stone', 'group:stone', 'group:stone'},
+		{'group:stick', 'group:stone', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:knife_stone',
+	recipe = {
+		{'', '', ''},
+		{'', 'group:stone', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:bigsword_steel',
+	recipe = {
+		{'', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'group:stick', 'default:steel_ingot', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:knife_steel',
+	recipe = {
+		{'', '', ''},
+		{'', 'default:steel_ingot', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:bigsword_bronze',
+	recipe = {
+		{'', 'default:bronze_ingot', 'default:bronze_ingot'},
+		{'default:bronze_ingot', 'default:bronze_ingot', 'default:bronze_ingot'},
+		{'group:stick', 'default:bronze_ingot', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:knife_bronze',
+	recipe = {
+		{'', '', ''},
+		{'', 'default:bronze_ingot', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:bigsword_mese',
+	recipe = {
+		{'', 'default:mese_crystal', 'default:mese_crystal'},
+		{'default:mese_crystal', 'default:mese_crystal', 'default:mese_crystal'},
+		{'group:stick', 'default:mese_crystal', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:knife_mese',
+	recipe = {
+		{'', '', ''},
+		{'', 'default:mese_crystal', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:bigsword_diamond',
+	recipe = {
+		{'', 'default:diamond', 'default:diamond'},
+		{'default:diamond', 'default:diamond', 'default:diamond'},
+		{'group:stick', 'default:diamond', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:knife_diamond',
+	recipe = {
+		{'', '', ''},
+		{'', 'default:diamond', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:knife_obsidian',
+	recipe = {
+		{'', '', ''},
+		{'', 'default:obsidian_shard', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:sword_gold',
+	recipe = {
+		{'default:gold_ingot'},
+		{'default:gold_ingot'},
+		{'group:stick'},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:knife_gold',
+	recipe = {
+		{'', '', ''},
+		{'', 'default:gold_ingot', ''},
+		{'group:stick', '', ''},
+	}
+})
+
+minetest.register_craft({
+	output = 'better_swords:bigsword_gold',
+	recipe = {
+		{'', 'default:gold_ingot', 'default:gold_ingot'},
+		{'default:gold_ingot', 'default:gold_ingot', 'default:gold_ingot'},
+		{'group:stick', 'default:gold_ingot', ''},
+	}
+})
+
